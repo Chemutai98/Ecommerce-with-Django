0f57556cc96d4ec5d2f0d66f74816476141506e5
Chemutai98 <melicynthia1998@gmail.com>
2024-06-16 12:24:28 +0300

    updated files

--- E_commerce/models.py
+++ E_commerce/models.py
@@ -7,6 +7,21 @@ class Product(models.Model):
     description =models.TextField(max_length=300)
     price =models.DecimalField(max_digits=9, decimal_places=2)
     image= models.ImageField(upload_to='products/')
+    Availability = models.BooleanField(default=True)
 
-class Details(models.Model):
-    pass
+def __str__(self):
+    return self.name
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null= True)
+    address = models.TextField(blank= True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email}"
+    
+
+
--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'E_commerce'
+
 urlpatterns = [
-    path('product_list', views.product_list, name ='product_list')
+    path('product_list/', views.product_list, name='product_list'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('customer_list/', views.customer_list, name='customer_list'),
+    path('customer_detail/<int:pk>/', views.customer_detail, name='customer_detail'),  # Added trailing slash
 ]
