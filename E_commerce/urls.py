d9269d24270deabb08be4f3d5e0492af83a54540
Chemutai98 <melicynthia1998@gmail.com>
2024-06-16 18:40:06 +0300

    added css files

--- E_commerce/urls.py
+++ E_commerce/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'E_commerce'
 
 urlpatterns = [
-    path('product_list/', views.product_list, name='product_list'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
-    path('customer_list/', views.customer_list, name='customer_list'),
-    path('customer_detail/<int:pk>/', views.customer_detail, name='customer_detail'),  # Added trailing slash
+    path('', views.home, name='home'),
+    path('products', views.product_list, name='product_list'),
+    path('products<int:pk>/', views.product_detail, name='product_detail'),
+    path('customer_list', views.customer_list, name='customer_list'),
+    path('customer_detail/<int:pk>/', views.customer_detail, name='customer_detail'),
 ]
--- E_commerce/views.py
+++ E_commerce/views.py
@@ -1,33 +1,37 @@
-from django.shortcuts import get_object_or_404, render 
+from django.shortcuts import get_object_or_404, render
 from .models import Customer, Product
 
+def home(request):
+    return render(request, 'E_commerce/home.html')
 
-# Create your views here.
+# View for listing products
 def product_list(request):
-    products = Product.objects.all
+    products = Product.objects.all()
     context = {
         'products': products
     }
-    return render(request,'E_commerce/product_list.html', context)
+    return render(request, 'E_commerce/product_list.html', context)
 
+# View for product details
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    context ={
-        'product' : product
+    context = {
+        'product': product
     }
-    return render(request, 'E_commmerce/product_detail.html', context)
+    return render(request, 'E_commerce/product_detail.html', context)
 
+# View for listing customers
 def customer_list(request):
-    customers =Customer.objects.all
-    context ={
+    customers = Customer.objects.all()
+    context = {
         'customers': customers
     }
     return render(request, 'E_commerce/customer_list.html', context)
 
+# View for customer details
 def customer_detail(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
-    context ={
-        'customer' : customer
+    context = {
+        'customer': customer
     }
-    return render( request, 'E_commerce/customer_detail.html', context)
-
+    return render(request, 'E_commerce/customer_detail.html', context)
